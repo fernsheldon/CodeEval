81f2ab33c68b4c1d6d2b123932a8672b9b97b845
dizzle <sheldon@Sheldons-MacBook-Air.local>
2015-06-27 22:15:42 +0530

    CodeEval_boilerPlate
    Boiler plate code for Code Eval solutions

---
+++ CodeEval_boilerTemplate/src/codeEval/solution/Main.java
@@ -0,0 +1,19 @@
+package codeEval.solution;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		
+		Scanner scanedFile = new Scanner(new File(args[0]));
+
+		while (scanedFile.hasNext()) {
+			
+		}
+		scanedFile.close();
+	}
+}
